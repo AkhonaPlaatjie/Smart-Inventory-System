5774264574400e7d9d7391cd7993ebc4982b61ca
akplajhb025 <akplajhb025@student.wethinkcode.co.za>
2026-04-20 17:11:11 +0200

    Added attributes

---
+++ src/main/java/za/co/akhona/InventoryManager.java
@@ -0,0 +1,5 @@
+package za.co.akhona;
+
+public class InventoryManager {
+
+}
---
+++ src/main/java/za/co/akhona/Item.java
@@ -0,0 +1,11 @@
+package za.co.akhona;
+
+public class Item {
+    public String name;
+    public double price;
+    public int quantity;
+    public float itemID;
+
+
+
+}
